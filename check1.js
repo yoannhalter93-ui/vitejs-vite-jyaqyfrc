f46697abf36dfeab31e13d638320b7f1f6b6d32c
yoannhalter93-ui <yoann.halter93@gmail.com>
2026-09-02 14:30:28 +0200

    Delete check1.js

--- check1.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const files = fs.readdirSync('src').filter(f => /juggle/i.test(f));
-console.log('FILES', files);
-const app = fs.readFileSync('src/App.tsx', 'utf8');
-const lines = app.split('\n');
-lines.forEach((l, i) => {
-  if (/JuggleGame/i.test(l)) console.log('APP_REF', i + 1, l.trim());
-});
