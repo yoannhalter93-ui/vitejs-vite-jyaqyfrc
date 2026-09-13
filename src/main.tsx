c79505bee569fba57b52598c25e072707796a56c
yoannhalter93-ui <yoann.halter93@gmail.com>
2026-09-13 22:07:45 +0200

    fix: import dashboard styles through Vite

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import './home-v2.css'
 import { AuthProvider } from './AuthContext'
 
 createRoot(document.getElementById('root')!).render(
