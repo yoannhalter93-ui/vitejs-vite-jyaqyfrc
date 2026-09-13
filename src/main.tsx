e9c499efd3c4b32a0126218a122438d6be93036d
yoannhalter93-ui <yoann.halter93@gmail.com>
2026-09-13 22:17:06 +0200

    style: load home detail layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import './home-v2.css'
+import './home-v2-details.css'
 import { AuthProvider } from './AuthContext'
 
 createRoot(document.getElementById('root')!).render(
