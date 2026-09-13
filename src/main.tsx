aa41ea6ec0e17be6ffd5452b7349363ba3828ee0
yoannhalter93-ui <yoann.halter93@gmail.com>
2026-09-13 22:45:18 +0200

    style: load exact mobile reference pass

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './home-v2.css'
 import './home-v2-details.css'
 import './home-visual-reference.css'
 import './home-reference-polish.css'
+import './home-reference-exact.css'
 import { AuthProvider } from './AuthContext'
 
 createRoot(document.getElementById('root')!).render(
