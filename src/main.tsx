9354c01825f76183ef8cc9c51d36b823e0cc5ce7
yoannhalter93-ui <yoann.halter93@gmail.com>
2026-09-13 22:39:02 +0200

    style: load final mobile polish after reference layer

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import './home-v2.css'
 import './home-v2-details.css'
 import './home-visual-reference.css'
+import './home-reference-polish.css'
 import { AuthProvider } from './AuthContext'
 
 createRoot(document.getElementById('root')!).render(
